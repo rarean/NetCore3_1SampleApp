2dfac849098d09add89354548533061128cfdbfc
Eric Helin <ehelin@felpsis.net>
2020-11-08 22:38:25 -0600

    update tu run on ubuntu

--- NetCore3_1SampleApp/Startup.cs
+++ NetCore3_1SampleApp/Startup.cs
@@ -26,6 +26,11 @@ namespace NetCore3_1SampleApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
